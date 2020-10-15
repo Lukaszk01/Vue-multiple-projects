b5fa795b69e4e0f35c5d6dc2421201b3ef157e06
Lukasz <lukaszkkoziel@gmail.com>
2020-10-15 18:09:37 +0900

    app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <Header />
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
 
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
           <QuestionBox
+            v-if="questions.length"
             :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
           />
         </b-col>
       </b-row>
@@ -22,24 +28,37 @@ export default {
   components: {
     Header,
     QuestionBox
-    // :currentQuestion="questions[index]"
   },
   data() {
     return {
       questions: [],
-      index: 0
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
     },
-[
-  {"category":"Animals","type":"multiple","difficulty":"medium","question":"What color colour is a polar bear&#039;s skin?","correct_answer":"Black","incorrect_answers":["White","Pink","Green"]}
-  ,{"category":"Animals","type":"multiple","difficulty":"easy","question":"How many legs do butterflies have?","correct_answer":"6","incorrect_answers":["2","4","0"]}
-  ,{"category":"Animals","type":"multiple","difficulty":"hard","question":"How many known living species of hyenas are there?","correct_answer":"4","incorrect_answers":["8","2","6"]},
-  {"category":"Animals","type":"multiple","difficulty":"medium","question":"Which animal was part of an Russian domestication experiment in 1959?","correct_answer":"Foxes","incorrect_answers":["Pigeons","Bears","Alligators"]},
-  {"category":"Animals","type":"multiple","difficulty":"hard","question":"What type of creature is a Bonobo?","correct_answer":"Ape","incorrect_answers":["Lion","Parrot","Wildcat"]},
-  {"category":"Animals","type":"multiple","difficulty":"medium","question":"What is the name of the family that the domestic cat is a member of?","correct_answer":"Felidae","incorrect_answers":["Felinae","Felis","Cat"]},
-  {"category":"Animals","type":"multiple","difficulty":"medium","question":"What dog bread is one of the oldest breeds of dog and has flourished since before 400 BCE.","correct_answer":"Pugs","incorrect_answers":["Bulldogs","Boxers","Chihuahua"]},
-  {"category":"Animals","type":"multiple","difficulty":"hard","question":"What is the name of the copper-rich protein that creates the blue blood in the Antarctic octopus?","correct_answer":"Hemocyanin","incorrect_answers":["Cytochrome","Iron","Methionine"]},
-  {"category":"Animals","type":"multiple","difficulty":"medium","question":"What is the collective noun for rats?","correct_answer":"Mischief","incorrect_answers":["Pack","Race","Drift"]},
-{"category":"Animals","type":"multiple","difficulty":"medium","question":"&quot;Decapods&quot; are an order of ten-footed crustaceans.  Which of these are NOT decapods?","correct_answer":"Krill","incorrect_answers":["Lobsters","Shrimp","Crabs"]}]}
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results
+      })
+  }
 }
 </script>
 
